d20487572b1945ad1b113a8c9858ca730558c15c
Hodan-1 <hodanabdi24@outlook.com>
2025-02-27 10:02:43 +0000

    MPI_CYTHON UPLOADED

--- mpi_cython.pyx
+++ mpi_cython.pyx
@@ -3,19 +3,17 @@ import numpy as np
 cimport numpy as np
 from libc.math cimport cos, exp
 from cython.parallel import prange
+from mpi4py import MPI
+
+# Initialize MPI
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
 
 ctypedef np.float64_t DTYPE_t
 
 ########################################
-cpdef inline double one_energy(double[:, :] arr, int ix, int iy, int sub_nmax, int nmax) nogil:
-    cdef int i
-    cdef int dx[4], dy[4]
-    cdef double en = 0.0, ang
-
-    dx[0], dy[0] = 1, 0
-    dx[1], dy[1] = -1, 0
-    dx[2], dy[2] = 0, 1
-    dx[3], dy[3] = 0, -1
+def one_energy(np.ndarray[DTYPE_t, ndim=2] arr, int ix, int iy, int sub_nmax, int nmax):
 
     # Use np.roll for periodic boundary conditions
     neighbors = (
@@ -29,28 +27,17 @@ cpdef inline double one_energy(double[:, :] arr, int ix, int iy, int sub_nmax, i
 ########################################
 def all_energy(np.ndarray[DTYPE_t, ndim=2] arr, int sub_nmax, int nmax):
 
-   """
+    """
     Compute the total energy of the lattice using vectorised operations.
     """
-
-neighbors = (
+    neighbors = (
         np.roll(arr, shift=1, axis=0) + np.roll(arr, shift=-1, axis=0) +
         np.roll(arr, shift=1, axis=1) + np.roll(arr, shift=-1, axis=1)
     )
     energy = 0.5 * (1.0 - 3.0 * np.cos(arr - neighbors) ** 2)
     return np.sum(energy)
 
-########################################
-cpdef get_order(np.ndarray[DTYPE_t, ndim=2] arr, int nmax):
-    """
-    Calculate the order parameter of the lattice.
-    """
 
-    Qab_local = np.sum(np.exp(1j * arr * 6))
-    Qab_total = comm.allreduce(Qab_local, op=MPI.SUM)
-    return np.abs(Qab_total) / (nmax * nmax)
-    
-    return np.linalg.eigvals(Qab).max()
 
 ########################################
 def MC_step(np.ndarray[DTYPE_t, ndim=2] arr, float Ts, int nmax):
@@ -60,7 +47,7 @@ def MC_step(np.ndarray[DTYPE_t, ndim=2] arr, float Ts, int nmax):
     scale = 0.1 + Ts
 
     # Generate random perturbations
-    angles = np.random.normal(scale=scale, size=arr.shape)
+    angles = np.random.normal(scale=scale, size=(arr.shape[0], arr.shape[1]))
     new_arr = arr + angles
 
     # Compute energy change (vectorized)
@@ -78,10 +65,40 @@ def MC_step(np.ndarray[DTYPE_t, ndim=2] arr, float Ts, int nmax):
     delta_E = 0.5 * (new_energy - old_energy)
 
     # Metropolis acceptance criterion (vectorized)
-    accept_mask = (delta_E <= 0) | (np.exp(-delta_E / Ts) >= np.random.rand(*arr.shape))
+    accept_mask = (delta_E <= 0) | (np.exp(-delta_E / Ts) >= np.random.rand(arr.shape[0], arr.shape[1]))
     arr[accept_mask] = new_arr[accept_mask]
 
     # Exchange boundaries
     exchange_boundaries(arr, nmax)
 
     return np.count_nonzero(accept_mask) / (arr.shape[0] * arr.shape[1])
+
+def exchange_boundaries(np.ndarray[DTYPE_t, ndim=2] arr, int nmax):
+    """
+    Exchange boundary rows with non-blocking MPI communication.
+    """
+    sub_nmax = arr.shape[0]
+    reqs = []
+
+    # Send to left, receive from right
+    if rank > 0:
+        reqs.append(comm.Isend(arr[0, :], dest=rank - 1))
+        reqs.append(comm.Irecv(arr[-1, :], source=rank - 1))
+
+    # Send to right, receive from left
+    if rank < size - 1:
+        reqs.append(comm.Isend(arr[-1, :], dest=rank + 1))
+        reqs.append(comm.Irecv(arr[0, :], source=rank + 1))
+
+    MPI.Request.Waitall(reqs)
+
+########################################
+def get_order(np.ndarray[DTYPE_t, ndim=2] arr, int nmax):
+    """
+    Calculate the order parameter of the lattice.
+    """
+
+    Qab_local = np.sum(np.exp(1j * arr * 6))
+    Qab_total = comm.allreduce(Qab_local, op=MPI.SUM)
+    return np.abs(Qab_total) / (nmax * nmax)
+
